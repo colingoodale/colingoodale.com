082d09cf394d7c193c4cc98361ef10ef4d25a50f
Colin <colingoodale@gmail.com>
2019-10-14 12:37:19 -0500

    added meta tagging for Facebook

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,5 +5,8 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "/index"));
     });
+    app.get("/picture", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/img/websiteimage.png"))
+    })
 
 };
